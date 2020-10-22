5cae8973dfea9cdd034af42857deb9b79efee09e
MarioSimou <marios.simouu@gmail.com>
2020-10-22 23:25:13 +0100

    feat: installing go

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+wget -O go1.15.3.linux-amd64.tar.gz https://dl.google.com/go/go1.15.3.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.15.3.linux-amd64.tar.gz
+echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+. ~/.bashrc
+
 go build -o ./bin/server ./cmd/server/main.go
 
 ./bin/server --PORT=80
