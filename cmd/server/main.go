29350f8649ac73c0f624254f62d4544c69c21e76
MarioSimou <marios.simouu@gmail.com>
2020-10-22 22:05:54 +0100

    feat: fixing .git

---
+++ cmd/server/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+
+	"../../internal"
+)
+
+
+
+func main(){
+	var port = flag.String("PORT", "3000", "The port the service listens to")
+
+	flag.Parse() 
+
+	var address = fmt.Sprintf(":%s", *port)
+
+
+	http.HandleFunc("/ping", internal.Ping)
+
+	fmt.Printf("The app listens on port %s\n", *port)
+	http.ListenAndServe(address, nil)
+}
