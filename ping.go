456242fb4611c07af6827d5f0b5421fbb01d6d31
MarioSimou <marios.simouu@gmail.com>
2020-10-22 22:04:12 +0100

    feat: initial commit

---
+++ ping.go
@@ -0,0 +1,10 @@
+package internal
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func Ping(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "pong")
+}
---
+++ ping_test.go
@@ -0,0 +1,27 @@
+package internal
+
+import (
+	"net/http/httptest"
+	"testing"
+)
+
+func TestPing(t *testing.T){
+	var w = httptest.NewRecorder()
+	var r = httptest.NewRequest("GET", "/ping", nil)
+
+	Ping(w,r)
+	var response = w.Result()
+
+	if response.StatusCode != 200 {
+		t.Errorf("Expected a status code of '200' rather than '%d'", response.StatusCode)
+	}
+
+	var body = make([]byte, 4)
+	if _, e := response.Body.Read(body); e != nil {
+		t.Errorf("Error: %v\n", e)
+	}
+
+	if s := string(body); s != "pong" {
+		t.Errorf("Expected string 'pong' rather than '%s'", s)
+	}
+}
