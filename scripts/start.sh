2c0cefb0edc007e8ce916a9eedd98aa837d2dc81
MarioSimou <marios.simouu@gmail.com>
2020-10-22 23:48:16 +0100

    feat: fixing blocking

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-wget -O go1.15.3.linux-amd64.tar.gz https://dl.google.com/go/go1.15.3.linux-amd64.tar.gz
-tar -C /usr/local -xzf go1.15.3.linux-amd64.tar.gz
-echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
-. ~/.bashrc
+# wget -O go1.15.3.linux-amd64.tar.gz https://dl.google.com/go/go1.15.3.linux-amd64.tar.gz
+# tar -C /usr/local -xzf go1.15.3.linux-amd64.tar.gz
+# echo "export PATH=$PATH:/usr/local/go/bin" >> ~/.bashrc
+# . ~/.bashrc
 
-cd /home/ec2-user/server
-ls -al
+# cd /home/ec2-user/server
 
 go build -o ./bin/server ./cmd/server/main.go
 
-./bin/server --PORT=80 &
+./bin/server --PORT=80 > /dev/null 2>&1 &
